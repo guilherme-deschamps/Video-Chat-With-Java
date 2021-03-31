a9898246904ea85aaa0c49efaadc37d04df08862
Guilherme Deschamps <guilhermerd6@hotmail.com>
2021-03-30 21:58:17 -0300

    ClientThread transformado em um JFrame para exibir os dados recebidos no Socket

--- src/Client/ClientThread.java
+++ src/Client/ClientThread.java
@@ -15,27 +15,50 @@ import java.net.Socket;
 public class ClientThread extends JFrame implements Runnable {
 
     private String ip;
+    private boolean running;
+    private BufferedImage bImage;
+    private ImageIcon icon;
+    private JPanel contentPane;
+    public static JLabel img_client_thread;
 
-    public ClientThread(String ip){
+    public ClientThread(String ip) {
         this.ip = ip;
+
+        EventQueue.invokeLater(() -> {
+            ClientThread frame = new ClientThread();
+            frame.setVisible(true);
+        });
+    }
+
+    public ClientThread() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 450, 300);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        contentPane.setLayout(new BorderLayout(0, 0));
+        setContentPane(contentPane);
+
+        img_client_thread = new JLabel("Client Thread IP: " + ip);
+        img_client_thread.setHorizontalAlignment(SwingConstants.CENTER);
+        contentPane.add(img_client_thread, BorderLayout.CENTER);
     }
 
     @Override
     public void run() {
+        running = true;
         try {
-            Socket s = new Socket(ip.split("/")[0], Integer.parseInt(ip.split("/")[1]));
-            ObjectOutputStream output = new ObjectOutputStream(s.getOutputStream());
+            Socket outputSocket = new Socket(ip.split(":")[0], Integer.parseInt(ip.split(":")[1]));
+            ObjectOutputStream output = new ObjectOutputStream(outputSocket.getOutputStream());
 
-            ImageIcon icon;
-            BufferedImage bImage;
+            ServerSocket inputServerSocket = new ServerSocket(7800);
+            Socket inputSocket = inputServerSocket.accept();
+            ObjectInputStream inputImage = new ObjectInputStream(inputSocket.getInputStream());
+
             Webcam cam = Webcam.getDefault();
             cam.open();
-            while (true) {
-                bImage = cam.getImage();
-                icon = new ImageIcon(bImage);
-                output.writeObject(icon);
-                output.flush();
-//                img_client.setIcon(icon);
+            while (running) {
+                sendImage(cam, output);
+                receiveImage(inputImage);
             }
         } catch (IOException e) {
             e.printStackTrace();
